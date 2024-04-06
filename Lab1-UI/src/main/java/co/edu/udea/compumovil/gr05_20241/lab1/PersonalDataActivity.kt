fe877de5fd1dd0639d963b7a9bbfeb772ccdccdb
Kevin-1515 <kevin.camona@udea.edu.co>
2024-04-06 04:42:35 -0500

    se crea el personaldata

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20241/lab1/PersonalDataActivity.kt
@@ -0,0 +1,2 @@
+package co.edu.udea.compumovil.gr05_20241.lab1
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Labs20241-Gr05"
-include(":Lab1-UI")
- 
+include(":Lab1-UI" )
