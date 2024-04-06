2c293b43ddf147c03b75483280518fff2ebc1fce
Carlos Andres Sanchez Aviles <carlos.sanchez7@udea.edu.co>
2024-04-06 01:28:22 -0500

    Se agregan cambio para evitar que el teclado oculte el lugar donde se escribe y se cambia el enter por siguente

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20241/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20241/lab1/ContactDataActivity.kt
@@ -1,8 +1,13 @@
 import android.util.Patterns
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
@@ -14,10 +19,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactDataActivity() {
     // Definir los campos de datos
@@ -50,7 +59,23 @@ fun ContactDataActivity() {
     }
 
     // Columna con los campos de entrada
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Informacion de contacto",
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
         CampoDeEntrada(
             value = telefono,
             onValueChange = { telefono = it },
@@ -123,7 +148,10 @@ fun CampoDeEntrada(
             value = value,
             onValueChange = onValueChange,
             label = { Text(label) },
-            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = ImeAction.Next // Esto cambiará el botón "Enter" por "Siguiente"
+            ),
             isError = isError,
             modifier = Modifier.weight(1f)
         )
