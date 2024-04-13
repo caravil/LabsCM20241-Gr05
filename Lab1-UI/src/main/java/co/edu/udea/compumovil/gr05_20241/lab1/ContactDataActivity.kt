af1715cc962872be4bfa7bfe0fd72631b9861183
Carlos Andres Sanchez Aviles <carlos.sanchez7@udea.edu.co>
2024-04-12 19:27:10 -0500

    cambios: Se agrega barra de navegación superior Se elimina boto para volver a la pagina anterior Se modulariza el código para evitar la duplicidad de código Se cambia el boton enter por siguiente Se valida que el teclado no ocupe el campo que se esta ingresando

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20241/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20241/lab1/ContactDataActivity.kt
@@ -1,178 +1,112 @@
+
 import android.content.res.Configuration
 import android.util.Patterns
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Place
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactDataActivity(navController: NavController) {
-    // Definir los campos de datos
-    var telefono by remember { mutableStateOf("") }
-    var direccion by remember { mutableStateOf("") }
-    var email by remember { mutableStateOf("") }
-    var pais by remember { mutableStateOf("") }
-    var ciudad by remember { mutableStateOf("") }
-
-    // Define un estado para controlar si los datos se enviaron correctamente
-    var datosEnviadosExitosamente by remember { mutableStateOf(false) }
-
-    // Variables para controlar los mensajes de error
-    var telefonoError by remember { mutableStateOf(false) }
-    var emailError by remember { mutableStateOf(false) }
-    var paisError by remember { mutableStateOf(false) }
-
-    // Iconos para los campos
-    val telefonoIcono = Icons.Filled.Phone
-    val direccionIcono = Icons.Filled.Home
-    val emailIcono = Icons.Filled.Email
-    val ciudadIcono = Icons.Filled.LocationOn
-    val paisIcono = Icons.Filled.Place
-
-    // Verifica si la orientación es landscape
     val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val scrollState = rememberScrollState()
 
-    // Función para validar los campos obligatorios
-    fun validarCamposObligatorios(): Boolean {
-        var isValid = true
-        telefonoError = telefono.isEmpty()
-        paisError = pais.isEmpty()
-        emailError = !Patterns.EMAIL_ADDRESS.matcher(email).matches()
-        isValid = isValid && !telefonoError && !emailError
-        return isValid
-    }
-
-    // Aquí puedes definir tus composiciones dependiendo de si estás en modo paisaje o no
-    if (isLandscape) {
-        // Composiciones específicas para orientación landscape
-    } else {
-        // Composiciones específicas para orientación portrait
-
-
-        // Columna con los campos de entrada
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Informacion de contacto",
-                        style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
-                    )
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            CampoDeEntrada(
-                value = telefono,
-                onValueChange = { telefono = it },
-                label = "Teléfono*",
-                keyboardType = KeyboardType.Phone,
-                icono = telefonoIcono,
-                isError = telefonoError
-            )
-            CampoDeEntrada(
-                value = direccion,
-                onValueChange = { direccion = it },
-                label = "Dirección",
-                icono = direccionIcono
-            )
-            CampoDeEntrada(
-                value = email,
-                onValueChange = { email = it },
-                label = "Email*",
-                keyboardType = KeyboardType.Email,
-                icono = emailIcono,
-                isError = emailError
-            )
-
-            CampoDeEntrada(
-                value = ciudad,
-                onValueChange = { ciudad = it },
-                label = "Ciudad",
-                keyboardType = KeyboardType.Email,
-                icono = ciudadIcono
-            )
-
-            CampoDeEntrada(
-                value = pais,
-                onValueChange = { pais = it },
-                label = "Pais*",
-                icono = paisIcono,
-                isError = paisError
-            )
-
+    Scaffold(
+        topBar = {
+            AppBar("Información de contacto", onNavigationClick = { navController.popBackStack() })
+        }
+    ) { innerPadding ->
+        if (isLandscape) {
             Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(scrollState)
             ) {
-                // Botón para enviar datos
-                Button(
-                    onClick = {
-                        if (validarCamposObligatorios()) {
-                            // Todos los campos requeridos están llenos, proceder con el envío
-                            // Actualizar el estado para indicar que los datos se han enviado con éxito
-                            datosEnviadosExitosamente = true
-                        } else {
-                            // Mostrar un mensaje de error indicando los campos que faltan o son inválidos
-                            // Esto podría hacerse mostrando un Toast, un Snackbar o simplemente actualizando un estado para mostrar un mensaje de error
-                        }
-                    }
-                ) {
-                    Text("Enviar")
+                Column(modifier = Modifier.weight(1f).padding(8.dp)) {
+                    ContactField("Teléfono*", Icons.Filled.Phone, KeyboardType.Phone)
+                    ContactField("Email*", Icons.Filled.Email, KeyboardType.Email)
+                    ContactField("Ciudad", Icons.Filled.LocationOn, KeyboardType.Text)
                 }
-
-                // Botón para retroceder sin enviar datos
-                Button(
-                    onClick = {
-                        // Puedes simplemente retroceder usando el NavController
-                        navController.popBackStack()
-                    }
-                ) {
-                    Text("Retroceder")
+                Column(modifier = Modifier.weight(1f).padding(8.dp)) {
+                    ContactField("Dirección", Icons.Filled.Home, KeyboardType.Text)
+                    ContactField("País*", Icons.Filled.Place, KeyboardType.Text)
+                    SubmitButton(navController)
                 }
             }
-        }
-
-        // Navegar a la pantalla inicial si los datos se enviaron correctamente
-        if (datosEnviadosExitosamente) {
-            // Utiliza una acción de navegación para volver a la página inicial
-            // Esto podría ser mediante un NavController u otra forma de navegación en tu aplicación
-            navController.navigate("ruta_a_la_pantalla_inicial")
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(scrollState)
+            ) {
+                ContactField("Teléfono*", Icons.Filled.Phone, KeyboardType.Phone)
+                ContactField("Dirección", Icons.Filled.Home, KeyboardType.Text)
+                ContactField("Email*", Icons.Filled.Email, KeyboardType.Email)
+                ContactField("Ciudad", Icons.Filled.LocationOn, KeyboardType.Text)
+                ContactField("País*", Icons.Filled.Place, KeyboardType.Text)
+                SubmitButton(navController)
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(title: String, onNavigationClick: () -> Unit) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Regresar")
+            }
+        }
+    )
+}
+
+@Composable
+fun ContactField(label: String, icon: ImageVector, keyboardType: KeyboardType) {
+    var value by remember { mutableStateOf("") }
+
+    val isError = remember { mutableStateOf(false) }
+
+    CampoDeEntrada(
+        value = value,
+        onValueChange = { newValue ->
+            value = newValue
+            isError.value = label.endsWith("*") && newValue.isEmpty()
+        },
+        label = label,
+        icono = icon,
+        keyboardType = keyboardType,
+        isError = isError.value // Aquí se usa isError.value para pasar el valor booleano a CampoDeEntrada
+    )
 }
 
 @Composable
@@ -200,9 +134,49 @@ fun CampoDeEntrada(
     }
 }
 
+@Composable
+fun SubmitButton(navController: NavController) {
+    Button(
+        onClick = {
+            // Aquí podrías obtener los valores de los campos de entrada y pasarlos a la función de validación
+            val isValid = validarCamposObligatorios(
+                telefono = "valor_telefono",
+                email = "valor_email",
+                pais = "valor_pais"
+            )
+            if (isValid) {
+                navController.navigate("MainActivity")
+            }
+        },
+        modifier = Modifier.padding(16.dp).fillMaxWidth()
+    ) {
+        Text("Enviar")
+    }
+}
+
+fun validarCamposObligatorios(telefono: String, email: String, pais: String): Boolean {
+    val telefonoValido = telefono.isNotBlank()
+    val emailValido = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    val paisValido = pais.isNotBlank()
+    // Otros campos opcionales pueden agregarse y validarse aquí según sea necesario
+    return telefonoValido && emailValido && paisValido
+}
+
 @Preview
 @Composable
 fun ContactDataPreview() {
-    val navController = rememberNavController() // NavController falso para la vista previa
+    val navController = rememberNavController() // NavController falso
     ContactDataActivity(navController = navController)
+}
+
+@Preview(name = "Portrait", widthDp = 360, heightDp = 640)
+@Composable
+fun ContactDataPreviewPortrait() {
+    ContactDataPreview()
+}
+
+@Preview(name = "Landscape", widthDp = 640, heightDp = 360)
+@Composable
+fun ContactDataPreviewLandscape() {
+    ContactDataPreview()
 }
